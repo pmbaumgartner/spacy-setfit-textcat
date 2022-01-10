22298ebcfd235431130b75cfe3a4836c88a483c1
Peter B <5107405+pmbaumgartner@users.noreply.github.com>
2022-01-09 22:33:12 -0500

    fixes

--- scripts/replace_config_section.py
+++ scripts/replace_config_section.py
@@ -14,7 +14,7 @@ def main(template: Path, input_cfg: Path):
     section_start, section_end = template.find("["), template.find("]") + 1
     section_str = template[section_start:section_end]
     section_str_re = re.escape(section_str)
-    section_regex = re.compile(fr"({section_str_re}[\s\S]*?)\[.+\]")
+    section_regex = re.compile(fr"({section_str_re}[\s\S]*?)(?=\[.+\])")
     replaced_cfg = re.sub(section_regex, template, cfg)
     Path(input_cfg).write_text(replaced_cfg)
 
